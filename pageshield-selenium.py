33b1b16cd630bb90815eb1acb17779c86b0e2a0f
ajiadiyudha <31063481+ajiadiyudha@users.noreply.github.com>
2024-12-19 17:59:42 +0800

    Update pageshield-selenium.py

--- pageshield-selenium.py
+++ pageshield-selenium.py
@@ -7,7 +7,7 @@ import argparse
 
 # Create an ArgumentParser to handle command-line arguments
 parser = argparse.ArgumentParser(description="Loop through a URL using Selenium.")
-parser.add_argument("url", nargs="?", default="https://pageshielddemo.mythingy.io/pageshieldforcecsp", help="URL to visit (default: https://pageshielddemo.mythingy.io/pageshieldforcecsp)")
+parser.add_argument("url", nargs="?", default="https://{change_into_your_domain}/pageshieldforcecsp", help="URL to visit (default: https://{change_into_your_domain}/pageshieldforcecsp)")
 parser.add_argument("num_loops", nargs="?", type=int, default=100, help="Number of times to loop through the URL with pageshieldforcecsp to force CSP (default: 100)")
 args = parser.parse_args()
 args = parser.parse_args()
